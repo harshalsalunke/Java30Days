64724e43632771a91dfdfd146174fc5fcec6ce54
Harshal <harshalsalunke1996@gmail.com>
2024-02-19 20:58:47 +0530

    Day3 code

---
+++ src/Day3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Day3 {
+
+	public static void main(String[] args) {
+		//Factorial Finder
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num=scan.nextInt();
+		int numCopy=num;
+		int factor=1;
+		// Iteratively calculate the factorial
+		if(num>=1) {
+		while(numCopy!=0)
+		{
+			factor=factor*numCopy;
+			numCopy--;
+		}
+		
+		System.out.println("Factorial of "+num+" is "+factor);
+		}
+		else
+			System.out.println("Number entered is negative");
+		
+		
+		
+
+	}
+
+}
