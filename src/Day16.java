1446508bcf7b80d34a102234bfb1e51d3f22a488
Harshal <harshalsalunke1996@gmail.com>
2024-03-03 11:40:34 +0530

    Day 16 commit

---
+++ src/Day16.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day16 {
+	public static void inputElements(ArrayList<Integer> arr, Scanner scan, int len) {
+        for (int i = 0; i < len; i++) {
+            arr.add(scan.nextInt());
+        }
+    }
+	public static int maximumElement(ArrayList<Integer> arr)
+	{
+		int max=Integer.MIN_VALUE;
+		for (int num:arr)
+		{
+			if(num>max) {
+				max=num;
+			}			
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Integer> arr=new ArrayList<Integer>();
+		
+		System.out.println("Enter length of the Array: ");
+		int len=scan.nextInt();
+		System.out.println("Enter elements of Array: ");
+		inputElements(arr, scan, len);
+		System.out.println("Max Element in Array is "+maximumElement(arr));
+		
+		
+
+	}
+
+}
